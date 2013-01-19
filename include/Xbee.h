94e7d87cf4f6bb87dfd2c662c69cf757ae4ff425
BananaSlug <jash@ucsc.edu>
2013-01-19 00:14:53 -0800

    These are my initial Xbee Files. I believe these are all the files I will need.
    Now that Uart.c is started I can begin implementing the correct functions into the Xbee.c. There will be updated again in the next few days.

---
+++ include/Xbee.h
@@ -0,0 +1,78 @@
+/*
+ * File: Xbee.h
+ *
+ * State machine for Xbee module.
+ *
+ */
+#ifndef Xbee_H
+#define Xbee_H
+
+#include "Error.h"
+#include "Util.h"
+
+/***********************************************************************
+ * PUBLIC DEFINITIONS                                                  *
+ ***********************************************************************/
+#define GROUND_STATION_XBEE 74
+#define BOAT_XBEE 33
+
+/**********************************************************************
+ * PUBLIC FUNCTIONS                                                   *
+ **********************************************************************/
+
+
+/**********************************************************************
+ * Function: Xbee_init()
+ * @param An options bitfield.
+ * @return none
+ * @remark Initializes the GPS module.
+ **********************************************************************/
+void Xbee_init(uint8_t options);
+
+/**********************************************************************
+ * Function: Xbee_isInitialized()
+ * @return Whether the GPS was initialized.
+ * @remark none
+ **********************************************************************/
+bool Xbee_isInitialized();
+
+/**********************************************************************
+ * Function: Xbee_programInit()
+ * @param Which Xbee is being programed, ground station or boat
+ * @remark Puts the Xbee into API mode.
+ **********************************************************************/
+void Xbee_programInit(uint8 whichXbee);
+
+/**********************************************************************
+ * Function: Xbee_isApi()
+ * @return Whether the Xbee was succesfully put into API mode
+ * @remark none
+ **********************************************************************/
+bool Xbee_isApi();
+
+/**********************************************************************
+ * Function: Xbee_programInit()
+ * @param Which Xbee is being programed, ground station or boat
+ * @remark Initializes an array with data required for sending a
+ *  message in API mode.
+ **********************************************************************/
+void Xbee_initSend(uint8 whichXbee);
+
+/**********************************************************************
+ * Function: Xbee_sendString()
+ * @param data that needs to be transmitted, split up into proper bytes
+ * @remark Adds data to the sendArray, than sends that data over the 
+ *  UART.
+ **********************************************************************/
+void Xbee_sendString(string data);
+
+
+/**********************************************************************
+ * Function: Xbee_recieveData()
+ * @remark Beings taking in data, once a packet has been fully read in,
+ * we will analyze that packet for it's useful information, namely
+ * the GPS cordinate we should be driving towards.
+ **********************************************************************/
+void Xbee_recieveData();
+
+#endif
