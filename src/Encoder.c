6b7f1211d18c1d925d2f9ee79e536cd24393c107
Darrel Deo <ddeo@ucsc.edu>
2013-02-13 23:26:21 -0800

    Fixed bug of measuring positive and negative pulsewidth in test harness. Need to fix for actual SM.

--- src/Encoder.c
+++ src/Encoder.c
@@ -6,7 +6,6 @@
  */
 
 #include <xc.h>
-//#include <p32xxxx.h>
 #include <stdio.h>
 #include <plib.h>
 #include "Serial.h"
@@ -31,25 +30,23 @@
 /***********************************************************************
  * PRIVATE DEFINITIONS                                                 *
  ***********************************************************************/
-/*
-#define FOSC           80E6 //80 MHz Clockrate
-#define PB_DIV         8    //Peripheral Bus Divider ==> 10Mhz
-#define PRESCALE       1    //Prescale 1:1 ==> 1 timer tick = 1 peripheral clk cycle
-#define MSEC           10E-3
-*/
 #define T3_TICK        0xFFFF// (500 * MSEC * FOSC)/(PB_DIV * PRESCALE)
 #define CaptureTimer   2
 
-#define PRINT_DELAY    1000//1 second
+#define PRINT_DELAY    100//1 second
 #define AngleScalar    2195E-6
 #define DEBUG
+
+/*DEFINE IO BUTTONS*/
+//LockOn Button
+#define LockOn_DIR PORTY05_TRIS
+#define LockOn PORTY05_BIT
+
 
 /***********************************************************************
  * PUBLIC FUNCTIONS                                                    *
  ***********************************************************************/
  void Encoder_init(){
-    
-
       //setup Timer 3
       OpenTimer3(T3_ON | T3_PS_1_1, T3_TICK);
 
@@ -62,7 +59,7 @@
 
       //Enable the Interrupt for input capture, set priority level 1
       ConfigIntCapture1(IC_INT_ON | IC_INT_PRIOR_1);
-      mIC1IntEnable(1);
+      mIC1IntEnable(0);
 
       //clear interrupt flag
       mIC1ClearIntFlag();
@@ -71,6 +68,14 @@
  float calculate_Angle(uint16_t pwidth){
      return (pwidth * AngleScalar);
  }
+
+ BOOL Is_lockOnButtonPressed(){
+     if(LockOn == 1)
+         return TRUE;
+     else
+         return FALSE;
+ }
+
 
 /***********************************************************************
  * GLOBAL VARIABLES                                                    *
@@ -84,14 +89,78 @@ static uint16_t start = 0;
 static uint16_t stop = 0;
 static uint16_t Pulse = 0;
 static uint16_t tempBuff = 0;
+static uint16_t accum_count = 0;
 static float    angle = 0;
 
+
 //State declarations
-enum state {calculate, poll};
+enum state {calculate, poll, event_checker, take_avg};
 static int16_t state = poll;
 
 /************************************************************************/
+//#define State_Machine
+#ifdef State_Machine
 
+int main(void){
+     //Module Initializations
+    Board_init();
+    Timer_init();
+    Serial_initSM();
+    Encoder_init();
+    LockOn_DIR = 1;
+    float accumulator;
+
+    state = event_checker;
+    while(1){
+        switch(state){
+            case(event_checker):
+                //
+                //printf("EventCheck\n\n");
+                if(Is_lockOnButtonPressed()){
+                    mIC1IntEnable(1);
+                    state = poll;
+                }
+
+            break;
+
+            case(calculate):
+                state = poll;
+                if(stop > start)
+                {
+                    Pulse = stop - start;
+                }else{
+                    Pulse = (T3_TICK - start) + stop;
+                }
+
+                accumulator += calculate_Angle(Pulse);
+                accum_count++;
+                if(accum_count == 300){
+                    mIC1IntEnable(0);
+                    angle = accumulator/300;
+                    printf("ANGLE: %.3f\n\n\n",angle);
+                    state = event_checker;
+                    accum_count = 0;
+                    accumulator = 0;
+                }
+                
+            break;
+
+            case(poll):
+                ;
+            break;
+
+
+        }
+
+
+    }
+
+
+
+
+
+}
+#endif
 
 #define Test_Harness
 #ifdef Test_Harness
@@ -102,13 +171,14 @@ int main(void){
     Timer_init();
     Serial_initSM();
     Encoder_init();
+    mIC1IntEnable(1);
 
 
    // printf("Encoder Test Harness!\n");
    Timer_new(TIMER_TEST,PRINT_DELAY);
 
 
-    //Port Y04 : J5-3, set for debug PWM
+    //Initialize PortY04 for digital input
     PORTY04_TRIS = 0;
     PORTY04_LAT = 0;
 
@@ -130,18 +200,13 @@ int main(void){
                 angle = calculate_Angle(Pulse);
                 //state = poll;
 
-                if(Timer_isExpired(TIMER_TEST)){
-                    while(!Serial_isTransmitEmpty()) {
-                     ;
-                    }
-                    printf("START: %u      STOP: %u     Pulse: %u      Angle: %.3f degrees\n\n\n",start,stop,Pulse,angle);
-                    //Serial_runSM();
-                    while(!Serial_isTransmitEmpty()) {
-                     ;
-                    }
-                    state = poll;
-                    mIC1IntEnable(1);                                           //Enable Interrupt
+                printf("START: %u      STOP:  %u     Pulse: %u      Angle: %.3f degrees\n\n\n", start, stop, Pulse, angle);
+                //Serial_runSM();
+                while (!Serial_isTransmitEmpty()) {
+                    ;
                 }
+                state = poll;
+
                 break;
 
             case(poll):
@@ -176,14 +241,17 @@ void __ISR( _INPUT_CAPTURE_1_VECTOR, ipl1) IC1Interrupt( void){
     //Falling Edge
     }else{
         PORTY04_LAT = 0;
+        //mIC1IntEnable(1);
         //Serial_putChar('F');                                                        //DEBUG PWM
         Rise = 0;                                                               //Set to fall
         state = calculate;                                                      //Change state to Calculate Pulsewidth
         stop = tempBuff;                                                        //Stop holds timer buffer of falling edge
-        //mIC1IntEnable(1);                                                       //Disable IC interrupt
-        Timer_new(TIMER_TEST,PRINT_DELAY);                                       //Set Timer for Prints
+                                                                                  //Disable IC interrupt
+      
     }
 
+
+    mIC1IntEnable(1);
     //Clear Interrupt Flag
     mIC1ClearIntFlag();
 }
