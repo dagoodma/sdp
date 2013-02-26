715357536b42ed27cecd3b1636edbb29e2be9d06
Bananaslug <cokepig2005@gmail.com>
2013-02-25 23:13:56 -0800

    Initial creation of Compass project

---
+++ include/Compass.h
@@ -0,0 +1,29 @@
+/*
+ * @file  Xbee.h
+ *
+ * @author John Ash
+ *
+ * @brief
+ * State machine for Xbee module.
+ *
+ * @details
+ * Module that wraps the bee in a statemachine that
+ * reads from the UART.
+ *
+ * @date February 1, 2013 2:59 AM -- created
+ *
+ */
+#ifndef Compass_H
+#define Compass_H
+
+/***********************************************************************
+ * PUBLIC DEFINITIONS                                                  *
+ ***********************************************************************/
+
+/**********************************************************************
+ * PUBLIC FUNCTIONS                                                   *
+ **********************************************************************/
+
+
+
+#endif
